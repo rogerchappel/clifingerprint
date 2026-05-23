9fed77c1571d09870ee4234506a9b809126baea7
Roger Chappel <roger@example.com>
2026-05-23 19:15:51 +1000

    feat: export public API from src/index.js

---
+++ src/fingerprint/index.js
@@ -0,0 +1,4 @@
+export { runProbe } from "./executor.js";
+export { loadConfig, validateConfig, buildFingerprint } from "./builder.js";
+export { compareFingerprints } from "./comparer.js";
+export { loadFingerprint, saveFingerprint, formatDiffReport, serializeFingerprint, parseFingerprint } from "./serializer.js";
