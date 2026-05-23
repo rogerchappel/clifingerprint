0b204f9e51c621ac9329335915ad733fc7449a9d
Roger Chappel <miscanalysis@gmail.com>
2026-05-23 19:08:26 +1000

    feat: export fingerprint module public API
    Re-exports types, executor, builder, config, serializer

---
+++ src/fingerprint/index.ts
@@ -0,0 +1,6 @@
+export { runProbe } from "./executor.js";
+export { loadConfig, validateConfig, buildFingerprint } from "./builder.js";
+export { compareFingerprints } from "./comparer.js";
+export { loadFingerprint, saveFingerprint, formatDiffReport, serializeFingerprint, parseFingerprint } from "./serializer.js";
+export type { ProbeConfig, ProbeResult, Fingerprint, CompareResult, ProbeDiff } from "./types.js";
+export type { ProbeConfigFile } from "./builder.js";
